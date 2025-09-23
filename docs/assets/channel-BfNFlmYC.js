f0b1e58bccbc6b6a799918fef46825fc63d29256
onshinpei <244221001@qq.com>
2025-09-23 21:30:39 +0800

    update docs

--- docs/assets/channel-BfNFlmYC.js
+++
@@ -1,1 +0,0 @@
-import{aH as o,aI as n}from"./main-hz97A0Cl.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ docs/assets/channel-Cw4vqvSG.js
@@ -0,0 +1,1 @@
+import{aH as o,aI as n}from"./main-mZBv7G4-.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
